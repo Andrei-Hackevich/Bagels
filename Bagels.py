00f09c533d9ef921b2ffd2a1a89cfeb73aa82e56
Андрей <andrei_hackevich@mail.ru>
2023-07-05 15:31:08 +0300

    игра готова

--- Bagels.py
+++ Bagels.py
@@ -1,11 +1,18 @@
 import random
 
-max_digit = 3 # количество цифр в загаданном числе
-max_guesses = 10 # количество попыток
+max_digit = 3  # количество цифр в загаданном числе
+max_guesses = 10  # количество попыток
 
+# Процесс игры
 def game():
     while True:
         num = get_num()
+        secret_num(num)
+        agree = input('Хотите сыграть еще? y/n: ')
+        if agree == 'n':
+            print('Приходите к нам еще!!!')
+            break
+
 
 # Функция, которая генерирует загаданное число
 def get_num():
@@ -17,8 +24,27 @@ def get_num():
         number = '0' + number
     return number
 
+# Процесс отгадки загаданного числа:
+def secret_num(num):
+    for i in range(0, max_guesses):
+        print(f'Попытка {i + 1}')
+        while len(choice) != max_digit or not choice.isdecimal():
+            choice = input('Введите ваше число: ')
+        if choice == num:
+            print('Вы угадали правильное число!!! Поздравляем!!!')
+            break
+        elif choice[0] in num or choice[1] in num or choice[2] in num:
+            for j in range(0, max_digit):
+                if choice[j] in num and choice[j] != num[j]:
+                    print('Pico', end=' ')
+                elif choice[j] == num[j]:
+                    print('Fermi', end=' ')
+            print('')
 
+        else:
+            print('Bagels')
 
+    if choice != num:
+        print(f'К сожалению Вы не угадали загаданное число: {num}')
 
-
-
+game()
